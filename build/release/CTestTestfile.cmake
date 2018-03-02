4c0625bff13e2c6f6a3e622886c9ddbaecff4ad3
Pubfred <pubfree@free.fr>
2018-03-02 15:39:47 +0000

    second

--- build/release/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/zeon
-# Build directory: /root/zeon/build/release
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(external)
-subdirs(src)
-subdirs(tests)
--- build/release/external/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/zeon/external
-# Build directory: /root/zeon/build/release/external
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(miniupnpc)
-subdirs(gtest)
